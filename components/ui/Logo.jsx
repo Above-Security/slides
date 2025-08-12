575845626b9d5ab91db3eeabc4ce8131c65e4631
AvivonAbove <aviv@abovesec.com>
2025-08-12 12:44:08 +0000

    fix: Make logo much smaller - reduced to minimal size

--- components/ui/Logo.jsx
+++ components/ui/Logo.jsx
@@ -10,10 +10,10 @@ export default function Logo({
     showText = false 
 }) {
     const sizeClasses = {
-        small: "w-8 h-8",
-        medium: "w-12 h-12",
-        large: "w-16 h-16",
-        xlarge: "w-20 h-20"
+        small: "w-4 h-4",
+        medium: "w-6 h-6",
+        large: "w-8 h-8",
+        xlarge: "w-10 h-10"
     };
 
     const logoSize = sizeClasses[size] || sizeClasses.large;
